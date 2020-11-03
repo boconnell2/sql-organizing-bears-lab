dea54f84332a5578efb16d6781078897ac6f89cb
boconnell2 <bmoconnell15@gmail.com>
2020-11-03 17:51:36 -0500

    updated bear code

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Blue", "Kind", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "M", "Blue", "Kind", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 12, "M", "Blue", "Kind", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "M", "Blue", "Kind", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "M", "Blue", "Kind", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "M", "Blue", "Kind", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "M", "Blue", "Kind", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 12, "M", "Blue", "Kind", true);
